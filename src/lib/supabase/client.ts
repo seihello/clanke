78eaff05b699cbaf3127561f3059525ed9711f24
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-12-18 22:15:24 -0800

    chore: create supabase clients

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,11 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+export default function createClient() {
+
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+
+  return supabase;
+}
